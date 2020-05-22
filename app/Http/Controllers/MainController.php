c5e338bfabb5e5cce34cedfc6cf6e9477c29592d
waruo <za100c80s116@yahoo.co.jp>
2020-05-22 17:53:13 +0900

    データ入力画面のバリテーションを追加

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,6 +7,7 @@ use App\Buki;
 use App\User;
 use App\Input_data;
 use Illuminate\Http\Request;
+use Validator;
 
 class MainController extends Controller
 {
@@ -26,6 +27,32 @@ class MainController extends Controller
         return view('input_data',['rules'=>$rules,'stages'=>$stages,'bukis'=>$bukis]);
     }
     public function input_data_complete(Request $request){
+        $rules = [
+            'rule_id' => 'required',
+            'stage1_id' => 'required',
+            'stage2_id' => 'required',
+            'buki_id' => 'required',
+            'xp' => 'numeric|between:1000,4000'
+        ];
+        $messages = [
+            'rule_id.required' => 'ルールを選択してください。',
+            'stage1_id.required' => 'ステージ1を選択してください。',
+            'stage2_id.required' => 'ステージ2を選択してください。',
+            'buki_id.required' => 'ブキを選択してください。' ,
+            'xp.numeric' => '数値を入力してください。',
+            'xp.between' => '1000～4000の間で入力してください。',
+            'stage1_id.different' => 'ステージ1とステージ2が重複しています。',
+            'stage2_id.different' => 'ステージ1とステージ2が重複しています。'
+        ];
+        $validator = Validator::make($request->all(),$rules,$messages);
+        $validator->sometimes('stage1_id','different:stage2_id',function($input){
+            //ステージ1とステージ2の両方が選択されている場合、ルールを追加
+            return !empty($input->stage1_id)&&!empty($input->stage2_id);
+        });
+        if($validator->fails()){
+            return redirect('/input_data')->withErrors($validator)->withInput();
+        }
+        
         $input_data = new input_data;
         $input_data->user_id = 1;
         $input_data->rule_id = $request->rule_id;
@@ -35,6 +62,7 @@ class MainController extends Controller
         $input_data->xp = $request->xp;
         $input_data->save();
         return redirect('/home');
+        
     }
     public function view_data(){
         $items = Input_data::all();
--- resources/views/input_data.blade.php
+++ resources/views/input_data.blade.php
@@ -9,40 +9,54 @@
     <h1>データ入力</h1>
     <form action="/input_data" method="post">
     @csrf
+    @error('rule_id')
+    <p>ERROR:{{$message}}</p>
+    @enderror
     <label for="rule_id">ルール</label>
     <select name="rule_id">
-    <option disabled selected></option>
+    <option value="" disabled selected>選択してください</option>
     @foreach($rules as $rule)
-        <option value="{{ $rule->rule_id }}">{{ $rule->rule_name }}</option>
+        <option value="{{ $rule->rule_id }}" @if(old('rule_id')==$rule->rule_id) selected @endif>{{ $rule->rule_name }}</option>
     @endforeach
     </select>
     <br>
+    @error('stage1_id')
+    <p>ERROR:{{$message}}</p>
+    @enderror
+    @error('stage2_id')
+    <p>ERROR:{{$message}}</p>
+    @enderror
     <label for="stage1_id">ステージ1</label>
     <select name="stage1_id">
-    <option disabled selected></option>
+    <option value="" disabled selected>選択してください</option>
     @foreach($stages as $stage)
-        <option value="{{ $stage->stage_id }}">{{ $stage->stage_name }}</option>
+        <option value="{{ $stage->stage_id }}" @if(old('stage1_id')==$stage->stage_id) selected @endif>{{ $stage->stage_name }}</option>
     @endforeach
     </select>
-    <br>
     <label for="stage2_id">ステージ2</label>
     <select name="stage2_id">
-    <option disabled selected></option>
+    <option value="" disabled selected>選択してください</option>
     @foreach($stages as $stage)
-        <option value="{{ $stage->stage_id }}">{{ $stage->stage_name }}</option>
+    <option value="{{ $stage->stage_id }}" @if(old('stage2_id')==$stage->stage_id) selected @endif>{{ $stage->stage_name }}</option>
     @endforeach
     </select>
     <br>
+    @error('buki_id')
+    <p>ERROR:{{$message}}</p>
+    @enderror
     <label for="buki_id">ブキ</label>
     <select name="buki_id">
-    <option disabled selected></option>
+    <option value="" disabled selected>選択してください</option>
     @foreach($bukis as $buki)
-        <option value="{{ $buki->buki_id }}">{{ $buki->buki_name }}</option>
+        <option value="{{ $buki->buki_id }}" @if(old('buki_id')==$buki->buki_id) selected @endif>{{ $buki->buki_name }}</option>
     @endforeach
     </select>
     <br>
+    @error('xp')
+    <p>ERROR:{{$message}}</p>
+    @enderror
     <label for="xp">ウデマエポイント</label>
-    <input type="text" name="xp">
+    <input type="text" name="xp" value="{{ old('xp') }}">
     <br>
     <input type="submit">
     </form>
