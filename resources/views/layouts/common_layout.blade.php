00c283d782e6cde624d7f2c8d9ed18a72c98b5b8
waruo <za100c80s116@yahoo.co.jp>
2020-11-10 11:12:11 +0900

    css,jsのパスの指定方法を変更

--- resources/views/layouts/common_layout.blade.php
+++ resources/views/layouts/common_layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <h1>@yield('h1')</h1>
@@ -35,7 +35,7 @@
     <div class="footer">
     copyright 2020 suto.
     </div>
-    <script src=" {{ mix('js/app.js') }} "></script>
+    <script src=" {{ asset('js/app.js') }} "></script>
     @yield('vue')
 </body>
 </html>
