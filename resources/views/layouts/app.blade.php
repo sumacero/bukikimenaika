782a73c0e1e9ba60f3dac899939baa289e96d4a2
waruo <za100c80s116@yahoo.co.jp>
2021-01-22 10:58:11 +0900

    GoogleAnalytics設置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QMZQM1WK6Z"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-QMZQM1WK6Z');
+    </script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="スプラトゥーン2,ガチマッチ,ステージ,ブキ,武器,ブキ選択,ウデマエ,上達">
